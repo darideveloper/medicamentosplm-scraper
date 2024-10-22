ce584046b4e5027fa94207e8436852147142e014
daridev <darideveloper@gmail.com>
2024-10-22 17:50:07 -0600

    feat (scraper): get page data

--- libs/scraper.py
+++ libs/scraper.py
@@ -3,16 +3,27 @@ from time import sleep
 import bs4
 import requests
 
+from libs.logs import logger
+
 
 class Scraper():
     
     def __init__(self):
         """ Load initial data and selectors """
         
+        logger.info("Initializing Scraper...")
         self.home_page = "https://medicamentosplm.com/Home/Medicamento"
         self.data = []
         self.selectors = {
             "page_btn": '.row + .container li.page-item',
+            "table_row": '.fila',
+            "table_columns": {
+                "medication": 'div:nth-child(1)',
+                "substance": 'div:nth-child(2)',
+                "pharmaceutical_form": 'div:nth-child(3)',
+                "laboratory": 'div:nth-child(4)',
+                "presentation": 'div:nth-child(5)',
+            }
         }
         
     def __get_page_soup__(self, page: int) -> bs4.BeautifulSoup:
@@ -39,7 +50,7 @@ class Scraper():
         res = requests.get(url, headers=headers)
         html = res.text
         soup = bs4.BeautifulSoup(html, 'html.parser')
-        sleep(2)
+        sleep(5)
         return soup
         
     def __get_max_pages_num__(self) -> int:
@@ -57,10 +68,41 @@ class Scraper():
         pages = len(page_btns) - 2
         return pages
     
-    def __get_page_data__(self) -> dict:
-        pass
+    def __get_page_data__(self, page: int) -> list[dict]:
+        """ Get data from a specific page of results
+
+        Returns:
+            list[dict]: The data table's page
+                medication: str
+                substance: str
+                pharmaceutical_form: str
+                laboratory: str
+                presentation: str
+        """
         
+        # Get page soup
+        soup = self.__get_page_soup__(page)
+        
+        # Loop table rows (skip header)
+        data = []
+        rows = soup.select(self.selectors["table_row"])
+        for row in rows[1:]:
+            
+            # Scrape row data
+            row_data = {}
+            table_columns_selecrtors = self.selectors["table_columns"]
+            for selector_name, selector_value in table_columns_selecrtors.items():
+                row_data[selector_name] = row.select_one(selector_value).text.strip()
+                
+            # Save row data
+            data.append(row_data)
+            
+        return data
+                
     def get_data(self):
         
         max_pages = self.__get_max_pages_num__()
-        print(max_pages)
+        for page in range(1, max_pages + 1):
+            logger.info(f"Getting data from page {page}...")
+            page_data = self.__get_page_data__(page)
+            print(page_data)
